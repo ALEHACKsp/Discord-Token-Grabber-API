9fdd4384fa80448627175747100451939e32edd9
cs <66363384+ecriminal@users.noreply.github.com>
2020-10-07 20:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     try:
         app = Flask(__name__)
         app.register_blueprint(routes)
-        app.run(port=config.webserver_port, debug=False)
+        app.run(host='0.0.0.0', port=config.webserver_port, debug=False)
     except Exception as e:
         print(f'An error occurred: {e}')
 
